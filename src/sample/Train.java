8d12635f3d944516c35075a197ddfd15cbecffcb
Alina Mihut <alinamihut@gmail.com>
2020-12-23 11:13:29 +0200

    added seat information

--- src/sample/Train.java
+++ src/sample/Train.java
@@ -6,8 +6,6 @@ import java.util.Arrays;
 
 public class Train {
     private String trainNumber;
-   // private String departureStation;
-    //private String arrivalStation;
     private Integer departureTime;
     private String[] stations;
     private Integer[] timeBetweenStations;
@@ -32,36 +30,53 @@ public class Train {
         return trainNumber;
     }
 
-    public void setTrainNumber(String trainNumber) {
-        this.trainNumber = trainNumber;
-    }
-
     public int getDepartureTime() {
         return departureTime;
-    }
-
-    public void setDepartureTime(int departureTime) {
-        this.departureTime = departureTime;
     }
 
     public Integer[] getTimeBetweenStations() {
         return timeBetweenStations;
     }
 
-    public void setTimeBetweenStations(Integer[] timeBetweenStations) {
-        this.timeBetweenStations = timeBetweenStations;
-    }
-
     public String[] getStations() {
         return stations;
+    }
+
+    public Integer getSeats1Class() {
+        return seats1Class;
+    }
+
+    public Integer getSeats2Class() {
+        return seats2Class;
+    }
+
+    public Integer getSeats1SleepingClass() {
+        return seats1SleepingClass;
+    }
+
+    public Integer getSeats2SleepingClass() {
+        return seats2SleepingClass;
+    }
+
+    public void setSeats1Class(Integer seats1Class) {
+        this.seats1Class = seats1Class;
+    }
+
+    public void setSeats2Class(Integer seats2Class) {
+        this.seats2Class = seats2Class;
+    }
+
+    public void setSeats1SleepingClass(Integer seats1SleepingClass) {
+        this.seats1SleepingClass = seats1SleepingClass;
+    }
+
+    public void setSeats2SleepingClass(Integer seats2SleepingClass) {
+        this.seats2SleepingClass = seats2SleepingClass;
     }
 
     public static void main(String[] args) throws IOException {
         ArrayList<Train> trains= new ArrayList<>();
         ArrayList<String> stations=new ArrayList<>();
-       //parseStationList(stations);
-       // createTrains(trains);
-       // printTrains(trains);
     }
 }
 
